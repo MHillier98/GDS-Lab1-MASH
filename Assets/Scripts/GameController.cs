b803e0f5e2eeea3016813f160f20e1c8cf700b37
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-02-25 00:08:36 +1100

    Added initial random generation (using perlin noise)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,6 +16,15 @@ public class GameController : MonoBehaviour
 
     private bool gameover = false;
 
+    [SerializeField]
+    public int WavesCompleted = 0;
+
+    [SerializeField]
+    public GameObject RockObject;
+
+    [SerializeField]
+    public GameObject SoldierObject;
+
     private void Start()
     {
         HoverText = gameObject.GetComponentInChildren<TextMeshPro>();
@@ -23,21 +32,21 @@ public class GameController : MonoBehaviour
         {
             HoverText.SetText("");
         }
+
+        GenerateMap(true, true);
     }
 
-    void Update()
+    private void Update()
     {
         if (!gameover)
         {
             GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-
             if (players.Length > 1)
             {
                 if (HoverText != null)
                 {
                     HoverText.SetText("Why is there more than one player?!");
                 }
-                //gameover = true;
                 return;
             }
             else if (players.Length <= 0)
@@ -51,14 +60,13 @@ public class GameController : MonoBehaviour
             }
 
             GameObject[] soldiers = GameObject.FindGameObjectsWithTag("Soldier");
-
             if (soldiers.Length <= 0)
             {
                 if (HoverText != null)
                 {
-                    HoverText.SetText("You win!");
+                    WavesCompleted += 1;
+                    GenerateMap(false, true); // spawn new soldiers
                 }
-                //gameover = true;
             }
         }
         else
@@ -68,6 +76,34 @@ public class GameController : MonoBehaviour
                 HoverText.SetText("");
                 Instantiate(Player, PlayerSpawnPosition, Quaternion.Euler(0, 90, 0));
                 gameover = false;
+            }
+        }
+    }
+
+    private void GenerateMap(bool generateRocks = false, bool generateSoldiers = false)
+    {
+        for (float x = -5.0f; x <= 22.0f; x += 0.5f)
+        {
+            for (float y = -13.0f; y <= 13.0f; y += 0.5f)
+            {
+                float newNoise = Random.Range(0.0f, 10000f);
+                float noise = Mathf.PerlinNoise(x + newNoise, y + newNoise);
+                if (noise >= 0.87f)
+                {
+                    if (RockObject != null && generateRocks)
+                    {
+                        float randomRotation = Random.Range(0, 360);
+                        Instantiate(RockObject, new Vector3(x, 0.0f, y), Quaternion.Euler(0, randomRotation, 0));
+                    }
+                }
+                else if (noise <= 0.05f)
+                {
+                    if (SoldierObject != null && generateSoldiers)
+                    {
+                        float randomRotation = Random.Range(0, 360);
+                        Instantiate(SoldierObject, new Vector3(x, 0.5f, y), Quaternion.Euler(90.0f, 0, 0));
+                    }
+                }
             }
         }
     }
