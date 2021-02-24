9147b6f247aa981604e07c20772bce51238d9b1a
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-02-24 13:26:39 +1100

    Added initial movement script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float MovementSpeed = 15.0f;
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxisRaw("Horizontal") * Time.deltaTime * MovementSpeed;
+        float verticalInput = Input.GetAxisRaw("Vertical") * Time.deltaTime * MovementSpeed;
 
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        transform.Translate(movement);
     }
 }
