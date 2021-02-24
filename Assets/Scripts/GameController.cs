6214414308c915cc0c3f030c12ca5b7f28dd15bf
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-02-24 19:18:09 +1100

    Added Game Over screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -23,6 +23,27 @@ public class GameController : MonoBehaviour
     {
         if (!gameover)
         {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+            if (players.Length > 1)
+            {
+                if (HoverText != null)
+                {
+                    HoverText.SetText("Why is there more than one player?!");
+                }
+                gameover = true;
+                return;
+            }
+            else if (players.Length <= 0)
+            {
+                if (HoverText != null)
+                {
+                    HoverText.SetText("Game over!");
+                }
+                gameover = true;
+                return;
+            }
+
             GameObject[] soldiers = GameObject.FindGameObjectsWithTag("Soldier");
 
             if (soldiers.Length <= 0)
