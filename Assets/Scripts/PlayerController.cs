f5ef280e76d5375c59592956eff99e7a981a8cbd
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-02-24 14:21:28 +1100

    Fixed player movement directions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal") * Time.deltaTime * MovementSpeed;
-        float verticalInput = Input.GetAxisRaw("Vertical") * Time.deltaTime * MovementSpeed;
+        float verticalInput = Input.GetAxisRaw("Vertical") * Time.deltaTime * -MovementSpeed;
 
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 movement = new Vector3(verticalInput, 0, horizontalInput);
         transform.Translate(movement);
     }
 
