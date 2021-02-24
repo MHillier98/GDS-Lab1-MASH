fda1242514a8ee047310ec4fc7dd993ac99d63ea
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-02-24 14:56:31 +1100

    Created Hospital with counter

---
+++ Assets/Scripts/HospitalController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HospitalController : MonoBehaviour
+{
+    public int SoldiersCollected = 0;
+
+    void Update()
+    {
+
+    }
+}
