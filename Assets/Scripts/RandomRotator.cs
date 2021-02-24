39e1ac374def68b3543f00bc808b86514be36f59
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-02-24 15:39:08 +1100

    Added rotation script for rocks + Custom Material

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    void Start()
+    {
+        float randomRotation = Random.Range(0, 360);
+        transform.Rotate(Vector3.down, randomRotation);
+    }
+}
